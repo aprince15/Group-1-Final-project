00359e2d703e2dda4ad9afaabb80a6f5eda41c2f
Anika Prince <anika.prince15@gmail.com>
2024-07-26 13:09:48 -0400

    Initial Commit

---
+++ Group 1 Final project/Group_1_Final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_1_Final_projectApp.swift
+//  Group 1 Final project
+//
+//  Created by Anika P on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Group_1_Final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
